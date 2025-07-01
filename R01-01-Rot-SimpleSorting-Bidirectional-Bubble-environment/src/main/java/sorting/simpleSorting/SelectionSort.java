f0edd4efb523f254d4d62d8702b483d22b3d6d89
joseparnaiba <jose.herlison.barroso.parnaiba@ccc.ufcg.edu.br>
2025-06-30 21:21:41 -0300

    Add files via upload

---
+++ R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/simpleSorting/SelectionSort.java
@@ -0,0 +1,32 @@
+package sorting.simpleSorting;
+
+import util.Util;
+import sorting.AbstractSorting;
+
+/**
+ * The selection sort algorithm chooses the smallest element from the array and
+ * puts it in the first position. Then chooses the second smallest element and
+ * stores it in the second position, and so on until the array is sorted.
+ */
+public class SelectionSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		
+		if(array == null || leftIndex < 0 || leftIndex >= rightIndex || rightIndex >= array.length){
+			return;
+		}
+
+		for(int i = leftIndex; i < array.length; i++){
+			int menor = i;
+
+			for(int j = i; j <= rightIndex; j++){
+				if (array[j].compareTo(array[menor])<0) {
+					menor = j;
+				}
+			}
+			Util.swap(array,i,menor);
+		}
+
+	}
+}
